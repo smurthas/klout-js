078522c56de746e5e834feecf23c5f9c6132327b
Simon Murtha-Smith <simon@murtha-smith.com>
2011-04-29 00:06:03 -0700

    It works!

---
+++ README.md
@@ -0,0 +1,31 @@
+# klout-js
+
+A simple wrapper of the Klout API in node
+
+    npm install klout-js
+
+## Usage
+
+First you need an API key, which you can get [here](http://developer.klout.com/) (but you already knew that).
+
+Then you can run the example to make sure everything is working:
+
+    node example.js your-api-key
+
+which looks something like this
+
+    var klout = require('klout-js')('your-api-key');
+
+    klout.klout(['smurthasmith', 'jeremie'], function(err, data) {
+        console.log('getKloutScore:', data);
+    });
+
+    klout.show(['smurthasmith', 'jeremie'], function(err, data) {
+        console.log('getKloutScore:', data);
+    });
+
+    klout.topics(['smurthasmith', 'jeremie'], function(err, data) {
+        console.log('getKloutScore:', data);
+    });
+    
+Follow [@smurthasmith](http://twitter.com/smurthasmith) for updates!
---
+++ klout-js.js
@@ -0,0 +1,37 @@
+var request = require('request');
+
+var BASE = 'http://api.klout.com/1/';
+
+module.exports = function(apiKey) {
+    var client = {API_KEY:apiKey};
+    
+    function makeRequest(endpoint, usernames, callback) {
+        request.get({uri:BASE + endpoint + '.json?key=' + client.API_KEY + '&users=' + usernames}, 
+            function(err, resp, body) {
+                if(err)
+                    callback(err, body);
+                else
+                    callback(null, JSON.parse(body));
+            });
+    }
+    
+    client.klout = function (usernames, callback) {
+        makeRequest('klout', usernames, callback);
+    };
+    client.show = function (usernames, callback) {
+        makeRequest('users/show', usernames, callback);
+    };
+    client.topics = function (usernames, callback) {
+        makeRequest('users/topics', usernames, callback);
+    };
+    //these endpoints don't seem to work, if anyone can get them to work, please help!!
+/*    client.influencedBy = function (usernames, callback) {
+        makeRequest('soi/influenced_by', usernames, callback);
+    };
+    client.influencerOf = function (usernames, callback) {
+        makeRequest('soi/influenced_of', usernames, callback);
+    };*/
+    
+    return client;
+}
+
