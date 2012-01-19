91faa36f84b9b5fd21f99215296ff7f47f4db840
Mehdi El Fadil <m.ef@mango-is.com>
2012-01-19 17:24:11 +0100

    corrected soi examples syntax + added comments and stringifyed query result outputs

--- example.js
+++ example.js
@@ -1,23 +1,29 @@
+// example usage of klout-js - queries one existing and one missing klout profiles
+// call providing your Klout API key as argument
+// eg. node example.js 1234abc
 var klout = require('./klout-js.js')(process.argv[2]);
 
 klout.klout(['smurthasmith', 'jeremie'], function(err, data) {
-    console.log('getKloutScore:', data);
+    console.log('getKloutScore:', JSON.stringify(data));
+    console.log('.....................');
 });
 
 klout.show(['smurthasmith', 'jeremie'], function(err, data) {
-    console.log('getKloutScore:', data);
+    console.log('getKloutProfile:', JSON.stringify(data));
+    console.log('.....................');
 });
 
 klout.topics(['smurthasmith', 'jeremie'], function(err, data) {
-    console.log('getKloutScore:', data);
+    console.log('getKloutTopics:', JSON.stringify(data));
+    console.log('.....................');
 });
 
-//these endpoints don't seem to work, if anyone can get them to work, please help!!
-/* 
-klout.influencedBy(['smurthasmith', 'jeremie'], function(data) {
-    console.log('getKloutScore:', data);
+klout.influencedBy(['smurthasmith', 'jeremie'], function(err, data) {
+    console.log('getKloutInfluencers:', JSON.stringify(data));
+    console.log('.....................');
 });
 
-klout.influencerOf(['smurthasmith', 'jeremie'], function(data) {
-    console.log('getKloutScore:', data);
-});*/
+klout.influencerOf(['smurthasmith', 'jeremie'], function(err, data) {
+    console.log('getKloutInfluencees:', JSON.stringify(data));
+    console.log('.....................');
+});
